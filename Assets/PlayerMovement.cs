d264d7997bebc204f4b612550fbbedb43cc9713e
Hezolite <64738931+Hezolite@users.noreply.github.com>
2023-04-05 19:20:04 +0500

    Movement update + ship sprite change
    Instead of MovePosition, AddForce is now being used to simulate inertia, more accurate gravitational forces.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,28 +4,61 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
     public Camera cam;
 
     public Rigidbody2D rb;
 
-    Vector2 movement;
+    public float maxVelocity = 10;
+
     Vector2 mousePos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        float axisX = Input.GetAxis("Horizontal");
+        float axisY = Input.GetAxis("Vertical");
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition); 
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
+        ThrustY(axisY);
+        ThrustX(axisX);
+
+        if (rb.velocity.sqrMagnitude > maxVelocity)
+            VelocityClamp();
+
+        RotationMouseControl();
     }
 
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
-        Vector2 lookDir = mousePos- rb.position;
+    private void VelocityClamp()
+    {
+        float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
+        float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
+
+        rb.velocity = new Vector2(x, y);
+    }
+
+    private void ThrustY(float amount)
+    { 
+        Vector2 forceY = transform.up * amount;
+
+        rb.AddForce(forceY);
+    }
+
+    private void ThrustX(float amount)
+    {
+        Vector2 forceX = transform.right * amount;
+
+        rb.AddForce(forceX);
+    }
+
+    private void RotationMouseControl()
+    {
+        Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
     }
